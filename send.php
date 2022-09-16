bd8615188cb306347c4ddc67f3cd37e27a998292
Rustam-Kafeev <63910460+Rustam-Kafeev@users.noreply.github.com>
2022-09-16 12:42:52 +0500

    Update send.php

--- send.php
+++ send.php
@@ -10,7 +10,7 @@ $email = trim($email);
 //echo $fio;
 //echo "<br>";
 //echo $email;
-if (mail("taxy-optima@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: info@vercel.app \r\n"))
+if (mail("taxy-optima@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: krr12@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
