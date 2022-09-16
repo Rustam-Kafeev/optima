47e795c77de0f39f95e07af05a8661a5e90dd372
Rustam-Kafeev <63910460+Rustam-Kafeev@users.noreply.github.com>
2022-09-16 12:47:44 +0500

    Update send.php

--- send.php
+++ send.php
@@ -7,9 +7,9 @@ $fio = urldecode($fio);
 $email = urldecode($email);
 $fio = trim($fio);
 $email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
+echo $fio;
+echo "<br>";
+echo $email;
 if (mail("taxy-optima@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: info@optima-rosy.vercel.app \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
